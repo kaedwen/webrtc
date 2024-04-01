94d02d846610d2dba1c64b22fb2d5b4f278e0e14
kaedwen <kaedwen@heinrich.blue>
2024-04-01 20:54:22 +0200

    added pwa

--- static/src/app/app.module.ts
+++ static/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SignalingService } from './services/signaling.service';
 import { VideoComponent } from './components/video/video.component';
 import { AudioComponent } from './components/audio/audio.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,12 @@ import { AudioComponent } from './components/audio/audio.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [SignalingService],
   bootstrap: [AppComponent]
